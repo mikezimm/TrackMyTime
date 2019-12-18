a4a511f333e0ee831d285f56c3f7a985a2aaebf6
MikeZimm <mike.zimmerman@autoliv.com>
2019-12-18 00:18:48 -0500

    Create state.entries object with arrays

--- src/webparts/trackMyTime/components/ITrackMyTimeState.ts
+++ src/webparts/trackMyTime/components/ITrackMyTimeState.ts
@@ -27,6 +27,32 @@ export interface IUser {
   id: any;
 }
 
+export interface IEntries {
+  entries: ITimeEntry[];
+}
+
+export interface IEntryInfo {
+
+  all: ITimeEntry[]; //All Entries
+  user: ITimeEntry[]; //Current user's entries
+  your: ITimeEntry[]; //Current user's entries
+  team: ITimeEntry[]; //Current user's entries
+  everyone: ITimeEntry[]; //Current user's entries
+  other: ITimeEntry[]; //Current user's entries
+
+  session? :ITimeEntry[]; //Session (page in browser) user's entries
+  today? : ITimeEntry[]; //Today's user's entries
+  week? : ITimeEntry[]; //This week's user's entries
+
+  userKeys: string[]; //Current user's entry keys
+  userPriority: ITimeEntry[]; //Current user's priority entries
+  current: ITimeEntry[]; //All 'Current' entries
+  lastFiltered: ITimeEntry[]; //Last filtered for search
+  lastEntry: ITimeEntry[];
+  newFiltered: ITimeEntry[]; //new filtered for search
+  
+}
+
 export interface ITimeEntry {
   //Values that would come from Project item
   id?: any; //Item ID on list
@@ -57,6 +83,7 @@ export interface ITimeEntry {
   startTime: any; //Time stamp
   endTime: any; // Time stamp
   duration?: any; //Number  -- May not be needed based on current testing with start and end dates.
+  age?: number; //Days since End Time
 
   //Saves what entry option was used... Since Last, Slider, Manual
   entryType?: string;
@@ -158,7 +185,7 @@ export interface IProjectInfo {
 export interface ITrackMyTimeState {
 
   projects?: IProjectInfo;
-
+  entries?: IEntryInfo;
   // 1 - Analytics options
   endTime?: theTime;
 
@@ -232,5 +259,3 @@ export interface ITrackMyTimeState {
 
 
 }
-
-
